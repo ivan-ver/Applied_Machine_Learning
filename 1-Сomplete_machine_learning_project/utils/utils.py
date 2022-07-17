14d0630b63442045233dc9c819b6560019d52a57
ivan_ver89 <ivan_ver89@mail.ru>
2022-07-17 23:35:38 +0300

    first

---
+++ 1-Сomplete_machine_learning_project/utils/utils.py
@@ -0,0 +1,28 @@
+import os
+import tarfile
+import urllib
+import pandas as pd
+import numpy as np
+
+
+def fetch_housing_data(housing_url, housing_path):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+    
+
+def loading_housing_data(housing_path):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+def split_train_test(data, test_ratio):
+    np.random.seed(42)
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
